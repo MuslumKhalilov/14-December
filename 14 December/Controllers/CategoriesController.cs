f5ff7368dac25055d9c0bf448d230e8a83a6b351
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-12-13 20:08:20 +0400

    HttpPut and HttpDelete actions created

--- 14 December/Controllers/CategoriesController.cs
+++ 14 December/Controllers/CategoriesController.cs
@@ -17,9 +17,9 @@ namespace _14_December.Controllers
             _context = context;
         }
 		[HttpGet]
-		public async Task<IActionResult> Get()
+		public async Task<IActionResult> Get(int page=1, int take=3)
 		{
-			List<Category> categories=await _context.Categories.ToListAsync();
+			List<Category> categories=await _context.Categories.Skip((page-1)*take).Take(take).ToListAsync();
 			return Ok(categories);
 		}
 		[HttpGet("{id}")]
@@ -42,6 +42,38 @@ namespace _14_December.Controllers
 			_context.Categories.Add(category);
 			await _context.SaveChangesAsync();
 			return StatusCode(StatusCodes.Status201Created,category);
+		}
+		[HttpPut]
+		public async Task<IActionResult> Update(int id,string name)
+		{
+			if (id <= 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest);
+			}
+			Category category = await _context.Categories.FirstOrDefaultAsync(c=>c.Id==id);
+			if (category == null)
+			{
+				return StatusCode(StatusCodes.Status404NotFound);
+			}
+			category.Name = name;
+			await _context.SaveChangesAsync();
+			return NoContent();
+		}
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete(int id)
+		{
+			if (id <= 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest);
+			}
+			Category category= await _context.Categories.FirstOrDefaultAsync(c=> c.Id==id);
+			if (category == null)
+			{
+				return StatusCode(StatusCodes.Status404NotFound);
+			}
+			_context.Categories.Remove(category);
+			await _context.SaveChangesAsync();
+			return NoContent();
 		}
     }
 }
