2d8d8dd348e71c0b562237e4470cf60c8d23cff2
MuslumKhalilov <tu6BG9HAZ@code.edu.az>
2023-12-13 19:48:02 +0400

    HttpPost and HttpGet actions created

--- 14 December/Controllers/CategoriesController.cs
+++ 14 December/Controllers/CategoriesController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _14_December.DAL;
+using _14_December.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _14_December.Controllers
 {
-	public class CategoriesController : Controller
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CategoriesController : ControllerBase
 	{
-		public IActionResult Index()
+		private readonly AppDbContext _context;
+
+		public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+		[HttpGet]
+		public async Task<IActionResult> Get()
 		{
-			return View();
+			List<Category> categories=await _context.Categories.ToListAsync();
+			return Ok(categories);
 		}
-	}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> Get(int id)
+		{
+			if (id <= 0)
+			{
+				return StatusCode(StatusCodes.Status400BadRequest);
+			}
+			Category category= await _context.Categories.FirstOrDefaultAsync(c=>c.Id==id);
+			if (category == null)
+			{
+				return StatusCode(StatusCodes.Status404NotFound);
+			}
+			return Ok(category);
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(Category category)
+		{
+			_context.Categories.Add(category);
+			await _context.SaveChangesAsync();
+			return StatusCode(StatusCodes.Status201Created,category);
+		}
+    }
 }
